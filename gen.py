d2786469008ff850462824a7816bc4afaf8e2dbd
Aleksandr Kosorukov <akosorukov90@gmail.com>
2020-12-13 15:54:04 +0300

    correction yield

--- gen.py
+++ gen.py
@@ -7,10 +7,10 @@ def md5_generator(path):
             md5_hash = hashlib.md5()
             md5_hash.update(string.encode('utf-8'))
             result = md5_hash.hexdigest()
-            yield print(result)
+            yield result
 
 
 if __name__ == '__main__':
     gen_md5 = md5_generator('result.txt')
     for line in gen_md5:
-        next(gen_md5)
+        print(line)
